ea64b0d6a170d9b4b182af78f5763108a0f9dcaa
wadevapp <wadevsollution@gmail.com>
2017-05-31 01:46:02 +0700

    add file index online shop

---
+++ index.php
@@ -0,0 +1,141 @@
+<?php
+
+	require_once('Library/Kohana/include.php');
+	require_once('Library/BinaryHelper.php');
+	$setting = ORM::factory('setting',1);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo"$setting->Name"?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+		<!-- bootstrap -->
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">      
+		<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+		
+		<link href="themes/css/bootstrappage.css" rel="stylesheet"/>
+		
+		<!-- global styles -->
+		<link href="themes/css/flexslider.css" rel="stylesheet"/>
+		<link href="themes/css/main.css" rel="stylesheet"/>
+
+		<!-- scripts -->
+		<script src="themes/js/jquery-1.7.2.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>				
+		<script src="themes/js/superfish.js"></script>	
+		<script src="themes/js/jquery.scrolltotop.js"></script>
+		<!--[if lt IE 9]>			
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+    <body>	
+		<div id="wrapper" class="container">
+			<section class="navbar main-menu">
+				<div class="navbar-inner main-menu">				
+					<a href="index.html" class="logo pull-left"><img src="themes/images/logo.png" class="site_logo" alt=""></a>
+					<nav id="menu" class="pull-right">
+						<ul>
+							<li><a href="./products.html">kategori</a>
+
+								<ul>
+									<?php foreach (ORM::factory('category')->find_all() as $category) { ?>
+										<li><a href="<?php echo '?category='.$category->Name; ?>"><?php echo"$category->Name"?></a></li>
+									<?php }?>							
+								</ul>
+							</li>																				
+							<li><a href="?category=Promo">Promo</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+			<section  class="homepage-slider" id="home-slider">
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+							<img src="themes/images/carousel/banner-1.jpg" alt="" />
+						</li>
+						<li>
+							<img src="themes/images/carousel/banner-2.jpg" alt="" />
+							<div class="intro">
+								<h1>Mid season sale</h1>
+								<p><span>Up to 50% Off</span></p>
+								<p><span>On selected items online and in stores</span></p>
+							</div>
+						</li>
+					</ul>
+				</div>			
+			</section>
+			<section class="main-content">
+			<?php if(isset($_GET['category']) && isset($_GET['id']) ): ?>
+				<?php include('detail.php') ?>
+			<?php else: ?>
+			<?php include('content.php') ?>
+			<?php endif ?>
+			</section>
+			
+			<section class="our_client">
+				<h4 class="title"><span class="text">Manufactures</span></h4>
+				<div class="row">					
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/14.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/35.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/1.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/2.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/3.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/4.png"></a>
+					</div>
+				</div>
+			</section>
+			<section id="footer-bar">
+				<div class="row">
+					<div class="span5">
+						<p class="logo"><img src="themes/images/logo.png" class="site_logo" alt=""></p>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. the  Lorem Ipsum has been the industry's standard dummy text ever since the you.</p>
+						<br/>
+						<span class="social_icons">
+							<a class="facebook" href="#">Facebook</a>
+							<a class="twitter" href="#">Twitter</a>
+							<a class="skype" href="#">Skype</a>
+							<a class="vimeo" href="#">Vimeo</a>
+						</span>
+					</div>					
+				</div>	
+			</section>
+			<section id="copyright">
+				<span>Copyright <?php echo date('Y') ?> All right reserved.</span>
+			</section>
+		</div>
+		<script src="themes/js/common.js"></script>
+		<script src="themes/js/jquery.flexslider-min.js"></script>
+		<script type="text/javascript">
+			$(function() {
+				$(document).ready(function() {
+					$('.flexslider').flexslider({
+						animation: "fade",
+						slideshowSpeed: 4000,
+						animationSpeed: 600,
+						controlNav: false,
+						directionNav: true,
+						controlsContainer: ".flex-container" // the container that holds the flexslider
+					});
+				});
+			});
+		</script>
+    </body>
+</html>
